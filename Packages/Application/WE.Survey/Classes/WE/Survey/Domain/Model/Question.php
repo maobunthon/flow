cd9ff5690b011ed513549cccde1de1d16a60be62
Bunthon Mao <bunthon@web-essentials.asia>
2015-07-29 13:32:54 +0700

    [TASK] Add flow file

---
+++ Packages/Application/WE.Survey/Classes/WE/Survey/Domain/Model/Question.php
@@ -0,0 +1,38 @@
+<?php
+namespace WE\Survey\Domain\Model;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "WE.Survey".             *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @Flow\Entity
+ */
+class Question {
+
+	/**
+	 * @var string
+	 */
+	protected $name;
+
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 * @return void
+	 */
+	public function setName($name) {
+		$this->name = $name;
+	}
+
+}
